aa8675f0e3b69417b3f11e862494b05f2b1e0eb0
Niko-CA <can25.union@gmail.com>
2024-11-28 14:42:12 -0300

    Cambios y nuevo repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
 
-import { TextField, Button, Container, Typography } from "@mui/material";
+import './App.css'
+import ButtonAppBar from './Components/ButtonAppBar/ButtonAppBar'
 
 function App() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    };
+
 
 
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h4" gutterBottom>
-        Formulario con Material UI
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Nombres"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          required
-        />
-        <TextField
-          label="Apellidos"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          required
-        />
-        <TextField
-          label="Correo electrónico"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          type="email"
-          required
-        />
-        <Button type="submit" variant="contained" color="primary">
-          Enviar
-        </Button>
-      </form>
-    </Container>
+    <>
+    <ButtonAppBar/>
+</>
   )
 }
 
